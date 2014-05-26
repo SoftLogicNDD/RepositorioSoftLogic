33c8ecb641291fa7e88879ab868a5ca732ac9623
arnaldo145 <arnaldomadeira11@hotmail.com>
2014-05-26 11:02:34 -0300

    Iniciando Menu Ajuda

--- RepositorioSoftLogic/Arnaldo/Program.cs
+++ RepositorioSoftLogic/Arnaldo/Program.cs
@@ -10,8 +10,19 @@ namespace Arnaldo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" ===== AJUDA =====");
+            Console.WriteLine(" ===== AJUDA =====\n");
+            MostrarOpcoes();
+            Console.Write("Informe a opção desejada: ");
+            int opcaoMenuAjuda = int.Parse(Console.ReadLine());
+            //switch (opcaoMenuAjuda)
+            //{
+            //}
+            Console.ReadKey();
+        }
 
+        static void MostrarOpcoes()
+        {
+            Console.WriteLine("1 - Cadastro \n2 - Consultas \n3 - Retornar ao Menu Inicial");
         }
     }
 }
